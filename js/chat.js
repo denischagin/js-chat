26e75a096bb2c3d441d774b9213ba92a05d8be37
denischagin <denischaginnn@gmail.com>
2022-04-01 00:30:45 +0300

    asdfasdf

--- js/chat.js
+++ js/chat.js
@@ -2,250 +2,180 @@ const chat = document.querySelector(".chat");
 
 // Сообщение бота
 const botMessage = (value) => {
-    if (value === '') {
-        return
-    }
-    divChat.insertAdjacentHTML(
+  if (value === "") {
+    return;
+  }
+  divChat.insertAdjacentHTML(
     "beforeEnd",
     `
-    <div class="message">
-        <h1>Bot</h1>
-        <p>${value}</p>
-    </div>
-    `
+	<div class="message">
+		<h1>Bot</h1>
+		<p>${value}</p>
+	</div>
+	`
   );
-  divChat.scrollTop=9999;
+  divChat.scrollTop = 9999;
 };
 
 // Сообщение пользователя
 const meMessage = (value) => {
-    divChat.insertAdjacentHTML(
+  divChat.insertAdjacentHTML(
     "beforeEnd",
     `
-    <div class="message">
-        <h1>Me</h1>
-        <p>${value}</p>
-    </div>
-`     
+	<div class="message">
+		<h1>Me</h1>
+		<p>${value}</p>
+	</div>
+`
   );
-  divChat.scrollTop=9999;
+  divChat.scrollTop = 9999;
 };
 // Переменные для игры
-let isPlaying = false
-let randomInt = Math.floor(Math.random() * 101)
+let isPlaying = false;
+let randomInt = Math.floor(Math.random() * 101);
 
 const divChat = document.querySelector(".chat");
 
+// Основные комманды бота
 const urlCommand = [
-    {command: '/vk', url: 'https://vk.com'},
-    {command: '/yt', url: 'https://youtube.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-    {command: '/gmail', url: 'https://gmail.com'},
-]
+  { command: "/vk", url: "https://vk.com" },
+  { command: "/yt", url: "https://youtube.com" },
+];
 
+document.getElementById("add-message").onclick = (e) => {
+  e.preventDefault();
+  const value = document.getElementById("form-for-message").value;
+  document.getElementById("form-for-message").value = "";
+  meMessage(value);
 
-// Комманды для бота
-document.getElementById("add-message").onclick = (e) => { 
-    e.preventDefault()
-    const value = document.getElementById("form-for-message").value;
-    document.getElementById("form-for-message").value = ''
-    meMessage(value)
+  urlCommand.forEach((command) => {
+    if (value.toLowerCase() === command.command) {
+      botMessage("Загрузка");
+      return window.open(command.url);
+    }
+  });
 
-    console.log(value)
+  // Логика игры
+  if (isPlaying === true) {
+    if (value.toLowerCase() === "/stop") {
+      botMessage("Игра остановлена");
+      return (isPlaying = false);
+    } else if (!isNaN(value)) {
+      if (Number(value) > randomInt && Number(value) <= 100) {
+        return botMessage("Введено большое число...");
+      } else if (Number(value) < randomInt && Number(value) >= 0) {
+        return botMessage("Введено маленькое число...");
+      } else if (Number(value) === randomInt) {
+        isPlaying = false;
+        randomInt = Math.floor(Math.random() * 101);
+        return botMessage("Вы угадали!");
+      } else {
+        return botMessage("Введено не правильное число");
+      }
+    } else {
+      return botMessage("Введено не правильное число");
+    }
+  }
 
-    urlCommand.forEach((command) => {
-        if (value.toLowerCase() === command.command) {
-            botMessage('Загрузка')
-            return window.open(command.url)
-        }
-    })
-    
-// Логика игры
-//     if (isPlaying === true) {
-//         if (value.toLowerCase() === '/stop') {
-//             botMessage('Игра остановлена')
-//             return isPlaying = false
-//         }
-//         else if (!isNaN(value)) {
-//             if (Number(value) > randomInt
-//             && Number(value) <= 100) {
-//                 return botMessage('Введено большое число...')
-//             }
-
-//             else if (Number(value) < randomInt 
-//             && Number(value) >= 0 ) {
-//                 return botMessage('Введено маленькое число...')
-//             }
-
-//             else if (Number(value) === randomInt ) {
-//                 isPlaying = false
-//                 randomInt = Math.floor(Math.random() * 101)
-//                 return botMessage("Вы угадали!")
-//             }
-
-//             else {
-//                 return botMessage('Введено не правильное число')
-//             }
-//         }
-//         else {
-//             return botMessage('Введено не правильное число')
-//         }
-//     }
-
-// // Комманды
-//     if (value.toLowerCase() === 'hello' 
-//     || value.toLowerCase() === 'привет') {
-//         return botMessage('Здорова')
-//     }
-
-//     else if (value.toLowerCase() === 'goodbye' 
-//     || value.toLowerCase() === 'by by'
-//     || value.toLowerCase() === 'bb' 
-//     || value.toLowerCase() === 'пока') {
-//         return botMessage('Bye bye')
-//     }
-    
-//     else if (value.toLowerCase() === '/startgame') { // Игра
-//         isPlaying = true
-//         return botMessage('Введите число от 1 до 100. <br>Напишите /stop чтобы закончить')
-//     }
-
-//     else if (value.toLowerCase() === '') {
-//         return botMessage('Не надо так')
-//     }
-
-//     else if (value.toLowerCase() === '/vk') {
-//         botMessage('Загрузка...')
-//         return window.open('https://vk.com') 
-//     }
-
-//     else if (value.toLowerCase() === '/yt') {
-//         botMessage('Загрузка...')
-//         return window.open('https://youtube.com') 
-//     }
-
-//     else if (value.toLowerCase() === 'хабиди ху') {
-//         return botMessage('Не дай бог')
-//     }
-
-//     else if (value.toLowerCase() === '/github') {
-//         botMessage('Загрузка')
-//         return window.open('https://github.com')
-//     }
-
-//     else{
-//         return botMessage("Хм.... Что это?")
-//     }
+  // Game
+  if (value.toLowerCase() === "/startgame") {
+    isPlaying = true;
+    return botMessage(
+      "Введите число от 1 до 100. <br>Напишите /stop чтобы закончить"
+    );
+  } 
 };
 
 // То что выводится в начале
-botMessage(`Вот мои возможности:
-<br>/startgame - игра, угадай цифру от 1 до 100
-<br>/vk
-<br>/yt
-<br>/github
-`)
+botMessage(`
+	Вот мои возможности:
+	${urlCommand.map((command) => {
+    return `<br>${command.command} - ${command.url}`;
+  })}`);
 
-const sideBar = document.getElementById("mySidenav")
-const menu = document.querySelector('.menu')
-const clearBtn = document.getElementById('clear-btn')
-const settingsBtn = document.getElementById('settings-btn')
-let isAppeared = true
+const sideBar = document.getElementById("mySidenav");
+const menu = document.querySelector(".menu");
+const clearBtn = document.getElementById("clear-btn");
+const settingsBtn = document.getElementById("settings-btn");
+let isAppeared = true;
 
 const click = (e) => {
-    if (isAppeared === false){
-        sideBar.style.width = "230px";
-        e.preventDefault()
-        isAppeared = true
-    }
-    else if (isAppeared === true) {
-        sideBar.style.width = "0";
-        e.preventDefault()
-        isAppeared = false
-    }
-}
+  if (isAppeared === false) {
+    sideBar.style.width = "230px";
+    e.preventDefault();
+    isAppeared = true;
+  } else if (isAppeared === true) {
+    sideBar.style.width = "0";
+    e.preventDefault();
+    isAppeared = false;
+  }
+};
+// Боковое меню
+sideBar.children["0"].addEventListener("click", click);
+menu.children["1"].addEventListener("click", click);
 
-sideBar.children['0'].addEventListener('click', click)
-menu.children['1'].addEventListener('click', click )
-console.log(menu)
+// Кнопка очистить на боковом меню
+clearBtn.addEventListener("click", () => {
+  document.location.reload();
+});
 
-clearBtn.addEventListener('click', () => {
-    document.location.reload()
-})
-settingsBtn.addEventListener('click', () => {
-    document.querySelector('.settings-unvisible').classList.add('settings-visible')
-    document.querySelector('.settings-unvisible').insertAdjacentHTML(
-        'beforeend',
-        `
-            <strong>Доступные комманды</strong>
-        `
-    )
-    urlCommand.forEach((command) => {
-        document.querySelector('.settings-unvisible').insertAdjacentHTML(
-            "beforeEnd",
-            `
-                <p>${command.command} - ${command.url}</p>
-            `     
-          );
-    })
-    document.querySelector('.settings-unvisible').insertAdjacentHTML(
-        'beforeend',
-        `
-        <form>
-            <input placeholder = 'Введите новую комманду' style='width: 200px'></ input>
-            <input placeholder = 'Введите url для комманды' style='width: 200px'></ input>
-            <button>Добавить</button>
-        </form>
-        `
-        )
+// Рендер окна settings
+settingsBtn.addEventListener("click", () => {
+  document
+    .querySelector(".settings-unvisible")
+    .classList.add("settings-visible");
+  document.querySelector(".commands").innerHTML = `
+			<strong>Доступные команды</strong>
+		`;
 
-})
+  urlCommand.forEach((command) => {
+    document.querySelector(".commands").insertAdjacentHTML(
+      "beforeEnd",
+      `
+				<p>${command.command} - ${command.url}</p>
+			`
+    );
+  });
+  document.querySelector(".settings-unvisible").insertAdjacentHTML(
+    "beforeend",
+    `
+		<form id='form-for-commands'>
+			<input id = 'input-for-command' placeholder = 'Введите новую комманду' ></ input>
+			<input id = 'input-for-url' placeholder = 'Введите url для комманды' value='https://'></ input>
+			<button> Добавить</button>
+		</form>
+		`
+  );
+
+  // Добавляем событие на кнопку добавить
+  document
+    .getElementById("form-for-commands")
+    .children["2"].addEventListener("click", (e) => {
+      e.preventDefault();
+      const command = document.getElementById("input-for-command");
+      const url = document.getElementById("input-for-url");
+
+      // Заполняем массив
+      console.log(command.value);
+      if (command.value) {
+        urlCommand.push({ command: command.value, url: url.value });
+        document.querySelector(".commands").insertAdjacentHTML(
+          "beforeEnd",
+          `
+					<p>${command.value} - ${url.value}</p>
+				`
+        );
+        botMessage(`
+				Вот мои возможности:
+				${urlCommand.map((command) => {
+          return `<br>${command.command} - ${command.url}`;
+        })}`);
+      }
+      // Скрываем окно настроек
+      document
+        .querySelector(".settings-unvisible")
+        .classList.remove("settings-visible");
+	 document.getElementById('form-for-commands').remove()
+	  
+    });
+});
